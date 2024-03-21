93d98f2dcb7abbd3bcaa0cc4c2ef2ca229046a51
haxxxs <99824472+haxxxs@users.noreply.github.com>
2024-03-21 18:03:00 +0400

    Basics props and onClick practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import Card from './components/Card'
-import Header from './components/Header'
-import Drawer from './components/Drawer';
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/imgs/sneakers/1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 15999,
+    imageUrl: "/imgs/sneakers/2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/imgs/sneakers/3.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/imgs/sneakers/4.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="wrapper clear">
-      <Drawer/>
-      <Header/>
+      <Drawer />
+      <Header />
       <div className="content p-40 ">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
@@ -16,11 +39,15 @@ function App() {
           </div>
         </div>
 
-        <div className="d-flex justify-between">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+        <div className="d-flex">
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick = {() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,21 +1,23 @@
-function Card() {
+import styles from './Card.module.scss'
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorie">
+    <div className={styles.card}>
+      <div className={styles.favorie}>
         <img img src="/imgs/heart-unliked.svg" alt="Unlicked"></img>
       </div>
-      <img height={112} width={133} src="/imgs/sneakers/1.jpg" alt="sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img height={112} width={133} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img width={11} height={11} src="/imgs/plus.svg" alt="Plus" />
         </button>
       </div>
-    </div>
+    </div>  
   );
 }
 
